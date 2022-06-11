37f2799211484440cfabc3b543f1cc89a321d725
Jay G <jay@hivegaming.com.au>
2022-06-11 12:04:45 +0800

    Ch1: Basic frontend pages and layouts

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -1,27 +1,13 @@
-import { Link, routes } from '@redwoodjs/router'
-import { MetaTags } from '@redwoodjs/web'
+import { MetaTags } from '@redwoodjs/web';
 
 const AboutPage = () => {
   return (
     <>
       <MetaTags title="About" description="About page" />
 
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <p>This site was created to demonstrate something...</p>
-        <Link to={routes.home()}>Return home</Link>
-      </main>
+      <p>This site was created to demonstrate something...</p>
     </>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,24 +1,12 @@
-import { Link, routes } from '@redwoodjs/router'
-import { MetaTags } from '@redwoodjs/web'
+import { MetaTags } from '@redwoodjs/web';
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>Home</main>
+      Home
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
