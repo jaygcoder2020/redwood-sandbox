c822768f8033eb2da991eee17ce04e3182b6cfd7
Jay G <jay@hivegaming.com.au>
2022-06-13 11:37:18 +0800

    Ch3: Form basics + GQL mutations

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -24,6 +24,7 @@ const Routes = () => {
         <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/hello" page={HomePage} name="home" />
+        <Route path="/contact" page={ContactPage} name="contact" />
       </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
